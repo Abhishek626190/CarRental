aed757d580fb1748c9bbf742816669ab51762bca
ABHISHEK RAI <raia81308@gmail.com>
2024-01-23 21:47:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function Suraj() {
+    // Specify the URL of the new page
+    var newPageURL = 'http://www.linkedin.com/in/suraj-rokade-4975ssr';
+
+    // Open a new window or tab with the specified URL
+    window.open(newPageURL, '_blank');
+}
+function Abhishek() {
+    // Specify the URL of the new page
+    var newPageURL = 'https://www.linkedin.com/in/abhishek-rai-241bab228/';
+
+    // Open a new window or tab with the specified URL
+    window.open(newPageURL, '_blank');
+}
+function Adarsh() {
+    // Specify the URL of the new page
+    var newPageURL = 'http://www.linkedin.com/in/suraj-rokade-4975ssr';
+
+    // Open a new window or tab with the specified URL
+    window.open(newPageURL, '_blank');
+}
+function Bhakti() {
+    // Specify the URL of the new page
+    var newPageURL = 'http://www.linkedin.com/in/suraj-rokade-4975ssr';
+
+    // Open a new window or tab with the specified URL
+    window.open(newPageURL, '_blank');
+}
